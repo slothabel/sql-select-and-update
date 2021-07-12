fc1dad401361d29a4efb182501bba57242fcac61
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-07-12 18:07:43 -0400

    Done.

---
+++ 01_insert_cats_into_cats_table.sql
@@ -0,0 +1,7 @@
+INSERT INTO cats (name, age, breed) VALUES ('Coconut', 6, 'Grey Tabby'); 
+
+-- UPDATE cats SET name = "Hannah" WHERE name = "Hana"; 
+
+
+
+-- SELECT * FROM cats;
---
+++ 02_add_column_to_cats.sql
@@ -0,0 +1,1 @@
+ALTER TABLE cats ADD COLUMN treat TEXT; 
